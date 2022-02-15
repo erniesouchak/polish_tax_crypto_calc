27408d93b76c21880928be90cc157bec53ce858d
erniesouchak <99737284+erniesouchak@users.noreply.github.com>
2022-02-15 15:08:28 +0100

    change currency to capital letters

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import requests
 value, currency_code = input("Podaj kwotę do przeliczenia. Np 12.5 CHF\n").split()
 url_template = f"http://api.nbp.pl/api/exchangerates/rates/a/{currency_code}"
 rate = requests.get(url_template).json()["rates"][0]["mid"]
-print(f"{value} {currency_code} = {rate * float(value)} PLN")
+print(f"{value} {currency_code.upper()} = {rate * float(value)} PLN")
